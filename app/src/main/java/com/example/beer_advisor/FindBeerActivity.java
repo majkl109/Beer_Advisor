813e3591517c67040c69c2c256b97d65615dc299
Majkl <majkl.andracic@gmail.com>
2020-02-10 20:38:16 +0100

    Activity added

--- app/src/main/java/com/example/beer_advisor/FindBeerActivity.java
+++ app/src/main/java/com/example/beer_advisor/FindBeerActivity.java
@@ -1,8 +1,12 @@
 package com.example.beer_advisor;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -11,4 +15,15 @@ public class FindBeerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    public void onClickFindBeer(View view){
+
+        TextView brands = findViewById(R.id.brands);
+        Spinner color = findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
+
+
+        }
+    }
+
