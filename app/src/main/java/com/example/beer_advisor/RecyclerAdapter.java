31c456cff97e70c4da1b8e6a5522922e73f15fac
Majkl <majkl.andracic@gmail.com>
2020-02-14 18:59:52 +0100

    Adapter

---
+++ app/src/main/java/com/example/beer_advisor/RecyclerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.beer_advisor;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter {
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    public static ImageViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView Album;
+        TextView AlbumTitle;
+
+        public ImageViewHoler(View itemView){
+            super(itemView);
+            Album = itemView.
+        }
+
+    }
+}
