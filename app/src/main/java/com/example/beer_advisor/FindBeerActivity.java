cd77e2838c97921ec5ebe9cdcfcb910d269896c3
Majkl <majkl.andracic@gmail.com>
2020-02-14 19:48:03 +0100

    Adapter

--- app/src/main/java/com/example/beer_advisor/FindBeerActivity.java
+++ app/src/main/java/com/example/beer_advisor/FindBeerActivity.java
@@ -1,14 +1,11 @@
 package com.example.beer_advisor;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Spinner;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
+
 
 
 public class FindBeerActivity extends AppCompatActivity {
@@ -16,9 +13,10 @@ public class FindBeerActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private int[] images ={R.drawable.bura, R.drawable.neposlusna, R.drawable.osjecko,
             R.drawable.ozujsko, R.drawable.ozujsko_amber, R.drawable.pan};
+    private RecyclerAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
 
-    private BeerExpert expert = new BeerExpert();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +24,13 @@ public class FindBeerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.recyclerView);
         layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerAdapter(images);
+        recyclerView.setAdapter(adapter);
     }
 
-    public void onClickFindBeer(View view){
 
-        TextView brands = findViewById(R.id.brands);
-        Spinner color = findViewById(R.id.color);
-        String beerType = String.valueOf(color.getSelectedItem());
-        brands.setText(beerType);
-        List<String> brandsList = expert.getBrands(beerType);
-        StringBuilder brandsFormatted = new StringBuilder();
-        for(String brand : brandsList){
-            brandsFormatted.append(brand).append('\n');
-        }
-        brands.setText(brandsFormatted);
+}
 
-
-        }
-    }
 
--- app/src/main/java/com/example/beer_advisor/RecyclerAdapter.java
+++ app/src/main/java/com/example/beer_advisor/RecyclerAdapter.java
@@ -5,20 +5,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerAdapter extends RecyclerView.Adapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<> {
 
     private int[] images;
     public RecyclerAdapter(int[] images){
         this.images = images;
     }
 
-    @NonNull
+
     @Override
-    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public Ima.onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext().inflate(R.layout.album_layout,parent))
         ImageViewHolder imageViewHolder = new ImageViewHolder(view);
 
@@ -26,13 +24,17 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+    public void onBindViewHolder(ImageViewHolder holder, int position) {
+
+        int image_id = images[position];
+        holder.Album.setImageResource(image_id);
+        holder.AlbumTitle.setText("Image: " + position);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return images.length;
     }
     public static ImageViewHolder extends RecyclerView.ViewHolder{
 
@@ -42,7 +44,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
         public ImageViewHoler(View itemView){
             super(itemView);
             Album = itemView.findViewById(R.id.album);
-            AlbumTitle = itemView.findViewById(R.album_title);
+            AlbumTitle = itemView.findViewById(R.id.album_title);
         }
 
     }
