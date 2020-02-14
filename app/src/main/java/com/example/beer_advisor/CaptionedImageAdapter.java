f6f9ea5cc18f5431319e3e1ac3f556bd5ccf92b8
Majkl <majkl.andracic@gmail.com>
2020-02-14 16:46:29 +0100

    Adapter

--- app/src/main/java/com/example/beer_advisor/CaptionedImageAdapter.java
+++ app/src/main/java/com/example/beer_advisor/CaptionedImageAdapter.java
@@ -1,9 +1,12 @@
 package com.example.beer_advisor;
 
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
     public class CaptionedImageAdapter extends
@@ -20,6 +23,17 @@ import androidx.recyclerview.widget.RecyclerView;
         public CaptionedImageAdapter(String[] captions, int[] imageIds) {
             this.captions = captions;
             this.imageIds = imageIds;
+
+            @Override
+            public void onBindViewHolder(ViewHolder holder, int position) {
+                CardView cardView = holder.cardView;
+                ImageView imageView = (ImageView)cardView.findViewById(R.id.info_image);
+                Drawable drawable = ContextCompat.getDrawable(cardView.getContext(),imageIds[position]);
+                imageView.setImageDrawable();
+                imageView.setContentDescription(captions[position]);
+                TextView textView = (TextView) cardView.findViewById(R.id.info_text);
+                textView.setText(captions[position]);
+            }
         }
 
         public static class ViewHolder extends RecyclerView.ViewHolder{
