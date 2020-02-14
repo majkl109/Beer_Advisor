78b81960508ff2763853859107995eb60f845f56
Majkl <majkl.andracic@gmail.com>
2020-02-14 18:50:16 +0100

    Pics

--- app/src/main/java/com/example/beer_advisor/FindBeerActivity.java
+++ app/src/main/java/com/example/beer_advisor/FindBeerActivity.java
@@ -5,16 +5,28 @@ import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
 
 
 public class FindBeerActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private int[] images ={R.drawable.bura, R.drawable.neposlusna, R.drawable.osjecko,
+            R.drawable.ozujsko, R.drawable.ozujsko_amber, R.drawable.pan};
+    private RecyclerView.LayoutManager layoutManager;
+
     private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
     }
 
     public void onClickFindBeer(View view){
