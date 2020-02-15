10a7d377c20dff0cfc2c4fcf539df3884d1ca6ec
Majkl <majkl.andracic@gmail.com>
2020-02-15 14:11:58 +0100

    Making code be(er)tter

--- app/src/main/java/com/example/beer_advisor/RecyclerAdapter.java
+++ app/src/main/java/com/example/beer_advisor/RecyclerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.beer_advisor;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,26 +8,33 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ImageViewHolder> {
 
-    private int[] images;
-    public RecyclerAdapter(int[] images){
-        this.images = images;
+    private List<Integer> beerView;
+    private List<String> beerName;
+    private LayoutInflater bInflater;
+    private ItemClickListener bClickListener;
+
+
+    RecyclerAdapter(Context context,List<Integer> beerView, List<String> beerName){
+        this.beerView = beerView;
+        this.beerName = beerName;
+        this.bInflater = LayoutInflater.from(context);
     }
 
 
     @Override
     public ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.album_layout,parent,false);
-        ImageViewHolder imageViewHolder = new ImageViewHolder(view);
-
-        return imageViewHolder;
+        View view = bInflater.inflate(R.layout.beer_layout,parent,false);
+        return new ImageViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ImageViewHolder holder, int position) {
 
-        int image_id = images[position];
+        int beer = beerView.get(position);
         holder.Album.setImageResource(image_id);
         holder.AlbumTitle.setText("Image: " + position);
 
@@ -36,7 +44,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ImageV
     public int getItemCount() {
         return images.length;
     }
-    public static class ImageViewHolder extends RecyclerView.ViewHolder{
+    public static class ImageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         ImageView Album;
         TextView AlbumTitle;
@@ -45,6 +53,12 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ImageV
             super(itemView);
             Album = itemView.findViewById(R.id.album);
             AlbumTitle = itemView.findViewById(R.id.album_title);
+            itemView.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View view){
+            if(mClickListener != null) mClickListener.onItemClick(view,getAdapterPosition());
+
         }
 
     }
