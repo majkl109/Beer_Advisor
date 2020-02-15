78d6dcbfdb4b003994b89a8e8f629dbcb6432925
Majkl <majkl.andracic@gmail.com>
2020-02-15 14:42:35 +0100

    Making code be(er)tter

--- app/src/main/java/com/example/beer_advisor/RecyclerAdapter.java
+++ app/src/main/java/com/example/beer_advisor/RecyclerAdapter.java
@@ -12,15 +12,15 @@ import java.util.List;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ImageViewHolder> {
 
-    private List<Integer> beerView;
-    private List<String> beerName;
+    private List<Integer> beerViews;
+    private List<String> beerNames;
     private LayoutInflater bInflater;
     private ItemClickListener bClickListener;
 
 
-    RecyclerAdapter(Context context,List<Integer> beerView, List<String> beerName){
-        this.beerView = beerView;
-        this.beerName = beerName;
+    RecyclerAdapter(Context context,List<Integer> beers, List<String> beerNames){
+        this.beerViews = beers;
+        this.beerNames = beerNames;
         this.bInflater = LayoutInflater.from(context);
     }
 
@@ -34,31 +34,43 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ImageV
     @Override
     public void onBindViewHolder(ImageViewHolder holder, int position) {
 
-        int beer = beerView.get(position);
-        holder.Album.setImageResource(image_id);
-        holder.AlbumTitle.setText("Image: " + position);
+        int beer = beerViews.get(position);
+        String nameOfBeer = beerNames.get(position);
+        holder.myImageView.setImage(beer);
+        holder.myTextView.setText(nameOfBeer);
 
     }
 
     @Override
     public int getItemCount() {
-        return images.length;
+        return beerNames.size();
     }
-    public static class ImageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class ImageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-        ImageView Album;
-        TextView AlbumTitle;
+        View myImageView;
+        TextView myTextView;
 
         public ImageViewHolder(View itemView){
             super(itemView);
-            Album = itemView.findViewById(R.id.album);
-            AlbumTitle = itemView.findViewById(R.id.album_title);
+            myImageView = itemView.findViewById(R.id.beer);
+            myTextView = itemView.findViewById(R.id.beer_name);
             itemView.setOnClickListener(this);
         }
         @Override
-        public void onClick(View view){
-            if(mClickListener != null) mClickListener.onItemClick(view,getAdapterPosition());
+        public void onClick(View view) {
+            if (bClickListener != null) bClickListener.onItemClick(view, getAdapterPosition());
+          }
+        }
+        public String getItem(int id){
+            return beerNames.get(id);
+        }
 
+        public void setClickListener(ItemClickListener itemClickListener){
+            this.bClickListener = itemClickListener;
+        }
+
+        public interface ItemClickListener{
+            void onItemClick(View view, int position);
         }
 
     }
