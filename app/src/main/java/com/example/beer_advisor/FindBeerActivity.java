bdfb9c14f8a3a023ba92e6350683b63836a8a299
Majkl <majkl.andracic@gmail.com>
2020-02-15 15:39:44 +0100

    Making code be(er)tter

--- app/src/main/java/com/example/beer_advisor/FindBeerActivity.java
+++ app/src/main/java/com/example/beer_advisor/FindBeerActivity.java
@@ -1,6 +1,9 @@
 package com.example.beer_advisor;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,23 +20,29 @@ public class FindBeerActivity extends AppCompatActivity implements RecyclerAdapt
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<Integer> viewBeers = new ArrayList<>();
-        viewBeers.add(Beer.)
 
 
+        ArrayList<String> beerNames = new ArrayList<>();
+        beerNames.add("Bura");
+        beerNames.add("Neposlusna");
+        beerNames.add("Osjecko");
+        beerNames.add("Ozujsko");
+        beerNames.add("Ozujsko_amber");
+        beerNames.add("Pan");
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.recyclerView);
-        layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(layoutManager);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager verticalLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(verticalLayoutManager);
+        adapter = new RecyclerAdapter(this, viewBers, beerNames);
         adapter = new RecyclerAdapter(images);
+        adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
     }
-
+    @Override
+    public void onItemClick(View view, int position){
+        Toast.makeText(this, "You clicked" + adapter.getItem(position)
+                + "on item position " + position, Toast.LENGTH_SHORT).show();
+    }
 
 }
 
